3b2f9ca6573bc2208d50822bf858708f926ee40d
Francis Duvivier <francisduvivier@gmail.com>
2019-03-31 11:25:40 +0200

    Improve argument passing and parsing and improve debug output

--- src/webviewSnapshot/WebViewSnapshotter.java
+++ src/webviewSnapshot/WebViewSnapshotter.java
@@ -19,101 +19,112 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 import static javafx.concurrent.Worker.State;
 
 public class WebViewSnapshotter extends Application {
     @NotNull
-    private final static String[] programArguments = new String[2];
-    private static final int EXTRA_PAGE_LOAD_TIME = 5000;
+    private static final int DEFAULT_EXTRA_PAGE_LOAD_TIME = 5000;
     private static boolean DEBUG_MODE = true;
     private static String DEMO_URL = "http://urlecho.appspot.com/echo?body=%3Ch1%3EPlease%20pass%20the%20URL%20as%20the%20first%20parameter%20to%20the%20program%3C/h1%3E";
     private static String DEMO_PATH = "screenshot.png";
 
     public static void main(String[] args) {
-        if (DEBUG_MODE) {
-            System.out.printf("Main application method called programArguments: [%s]%n", args);
-        }
-//        if (args.length < 2) {
-//            System.out.printf("No outputPath argument was passed. The outputPath should be passed as the second argument, using  default path [%s] instead%n", DEMO_PATH);
-//            programArguments[1] = DEMO_PATH;
-//        } else {
-//            programArguments[1] = args[1];
-//        }
-        if (args.length < 1) {
-            System.out.printf("No outputPath argument was passed. The outputPath should be passed as the second argument, using  Demo Url instead%n");
-            programArguments[0] = DEMO_URL;
-        } else {
-            programArguments[0] = args[0].split(",")[0];
-            programArguments[1] = args[0].split(",")[1];
-        }
-
-
         Application.launch(args);
     }
 
     @Override
     public void start(final Stage stage) {
+
+        Map<String, String> namedParameters = getParameters().getNamed();
+        if (DEBUG_MODE) {
+            System.out.printf("Application 'start' method called with parameters: [%s]%n", namedParameters);
+        }
+        String outputPath = namedParameters.get("path");
+        if (outputPath == null) {
+            System.out.printf("WARNING: No path argument was passed. The outputPath should be passed as with --path=path/to/screenshot.png. Will default path [%s] instead%n", DEMO_PATH);
+            outputPath = DEMO_PATH;
+        }
+
+        String url = namedParameters.get("url");
+        if (url == null) {
+            System.out.printf("WARNING: No url argument was passed. The url should be passed with --url=http://example.com, now using Demo Url instead%n");
+            url = DEMO_URL;
+        }
+
+        int tempExtraPageLoadTime = -1;
+        String extraTimeArgument = namedParameters.get("extraTime");
+
+        try {
+            tempExtraPageLoadTime = Integer.parseInt(extraTimeArgument);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (tempExtraPageLoadTime == -1) {
+            System.out.printf("WARNING: No or bad extraTime argument was passed. The extraTime in milliseconds can be passed with eg. --extraTime=4000 for 4 seconds, now using default: %s millis%n", DEFAULT_EXTRA_PAGE_LOAD_TIME);
+            tempExtraPageLoadTime = DEFAULT_EXTRA_PAGE_LOAD_TIME;
+        }
+        final int extraPageLoadTime = tempExtraPageLoadTime;
+        try {
+            url = new URL(url).toString();
+        } catch (MalformedURLException e) {
+            System.out.printf("WARNING: The given url is invalid, now using Demo Url instead%n");
+            e.printStackTrace();
+            url = DEMO_URL;
+        }
+
         // Create the WebView
         WebView webView = new WebView();
         // Create the WebEngine
         final WebEngine webEngine = webView.getEngine();
 
-        // Load the Page
-        String url = null;
-        try {
-            URL url1 = new URL(programArguments[0]);
-
-            url = url1.toString();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        String pathLocation = programArguments[1];
-        File outputFile = new File(pathLocation);
-        System.out.println(outputFile.getAbsolutePath());
+        // Try to create the output file directory
+        File outputFile = new File(outputPath);
         File dir = outputFile.getAbsoluteFile().getParentFile();
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
+        dir.mkdirs();
 
+        // Load the Page
         webEngine.load(url);
 
         // Update the stage title when a new web page title is available
         webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
             public void changed(ObservableValue<? extends State> ov, State oldState, State newState) {
                 if (DEBUG_MODE) {
-                    System.out.printf("Received page state: [%s]%n", newState);
+                    System.out.printf("Received page state: [%s], now waiting for %s seconds to make screenshot%n", newState, extraPageLoadTime / 1000D);
                 }
                 if (newState == State.SUCCEEDED) {
 
                     new Thread(() -> {
                         try {
-                            Thread.sleep(EXTRA_PAGE_LOAD_TIME); //Wait extra because async javascript might still be executing and changing the page.
+                            Thread.sleep(extraPageLoadTime); //Wait extra because async javascript might still be executing and changing the page.
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                         Platform.runLater(() -> {
                             doSnapshot(webView, outputFile);
                             if (DEBUG_MODE) {
-                                System.out.println("Did snapshot");
+                                System.out.printf("Screenshot made and saved to [%s]%n", outputFile.getAbsolutePath());
                             }
                         });
                     }).start();
                 }
             }
         });
+        startWebviewRendering(stage, webView);
+    }
+
+    private void startWebviewRendering(Stage stage, WebView webView) {
         // Create the VBox
         VBox root = new VBox();
-
         // Add the WebView to the VBox
         root.getChildren().add(webView);
-
         Scene scene = new Scene(root);
         // Add  the Scene to the Stage
         stage.setScene(scene);
-        // Display the Stage
+        // Display the Stage (Headless because we use Monocle)
         stage.show();
-
     }
 
     private void doSnapshot(WebView webView, File outputFile) {
