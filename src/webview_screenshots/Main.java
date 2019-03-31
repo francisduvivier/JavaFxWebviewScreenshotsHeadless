d1319eaf81f12f69d6f1909e52d576388e0094d0
Francis Duvivier <francisduvivier@gmail.com>
2019-04-01 00:56:37 +0200

    Cleanup some spacing inconsistencies

--- src/webview_screenshots/Main.java
+++ src/webview_screenshots/Main.java
@@ -53,6 +53,4 @@ public class Main extends Application {
             Platform.exit();
         }));
     }
-
-
 }
--- src/webview_screenshots/StaticConfig.java
+++ src/webview_screenshots/StaticConfig.java
@@ -5,5 +5,4 @@ public class StaticConfig {
     public static final boolean DEBUG_MODE = false;
     public static final String DEMO_URL = "http://urlecho.appspot.com/echo?body=<h1>Please pass a valid url parameter!</h1>";
     public static final String DEMO_PATH = "screenshot.png";
-
 }
